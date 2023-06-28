2ebbdac1a7bd5b4c1cce5669233458c15eb803f7
Soye Park <stardust6653@gmail.com>
2023-06-28 19:12:45 +0900

    feat : 중앙정렬을 위한 CenterAlignment 추가

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import Header from "components/Header";
 import "./globals.css";
 import Banner from "components/Banner";
 import PointLine from "components/PointLine";
+import Introduce from "components/Introduce";
+import Footer from "components/Footer";
+import CenterAlignment from "components/layout/CenterAlignment";
 
 function App() {
   return (
@@ -9,10 +12,15 @@ function App() {
       <header>
         <Header />
       </header>
-      <main>
+      <CenterAlignment>
         <Banner />
         <PointLine />
-      </main>
+        <Introduce />
+      </CenterAlignment>
+
+      <footer>
+        <Footer />
+      </footer>
     </div>
   );
 }
