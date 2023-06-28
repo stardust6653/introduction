e94e8cc25758285ff36584ed9596d357900040e9
Soye Park <stardust6653@gmail.com>
2023-06-28 19:11:28 +0900

    feat : 중앙 정렬을 위한 레이아웃 컴포넌트 추가

---
+++ src/components/layout/CenterAlignment.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { styled } from "styled-components";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const CenterAlignment = ({ children }: Props) => {
+  return <Alignment>{children}</Alignment>;
+};
+
+const Alignment = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default CenterAlignment;
