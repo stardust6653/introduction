41722de49629fc4f4784afd82b0fbbe0e64d421e
Soye Park <stardust6653@gmail.com>
2023-06-28 19:13:40 +0900

    feat : footer 기본 레이아웃

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { styled } from "styled-components";
+
+const Footer = () => {
+  return <FooterLayout>Footer</FooterLayout>;
+};
+
+const FooterLayout = styled.div`
+  width: 100%;
+  height: 300px;
+  background-color: #172027;
+  margin-top: 50px;
+`;
+
+export default Footer;
