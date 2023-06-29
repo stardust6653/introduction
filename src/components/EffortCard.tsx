7240d4f7fb7332c840447baf917e5f80ad22e7a7
Soye Park <stardust6653@gmail.com>
2023-06-29 23:56:34 +0900

    feat : Effort List 및 Card 컴포넌트 마크업 및 스타일링

---
+++ src/components/EffortCard.tsx
@@ -0,0 +1,40 @@
+import { EffortProps } from "data/effortData";
+import React from "react";
+import { styled } from "styled-components";
+
+type Props = {
+  post: EffortProps;
+};
+
+const ActiveCard = ({ post }: Props) => {
+  console.log(post.title);
+  return (
+    <>
+      <ActiveCardComponent key={post.title}>
+        <CardTitle>{post.title}</CardTitle>
+        <CardDescription>{post.description}</CardDescription>
+      </ActiveCardComponent>
+    </>
+  );
+};
+
+export default ActiveCard;
+
+const ActiveCardComponent = styled.div`
+  border-bottom: 1px solid black;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  text-align: center;
+  padding: 3rem;
+`;
+
+const CardTitle = styled.h4`
+  font-size: 1.2rem;
+  font-weight: 700;
+  margin-bottom: 1rem;
+`;
+
+const CardDescription = styled.p`
+  line-height: 1.4rem;
+  text-align: justify;
+`;
---
+++ src/components/EffortList.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import Title from "./ui/Title";
+import EffortCard from "./EffortCard";
+import { styled } from "styled-components";
+import { EffortProps, effortData } from "data/effortData";
+
+const EffortList = () => {
+  return (
+    <EffortListComponent>
+      <Title title={"<Effort/>"} />
+      <CardListComponent>
+        {effortData.map((item: EffortProps) => {
+          return <EffortCard post={item} />;
+        })}
+      </CardListComponent>
+    </EffortListComponent>
+  );
+};
+
+export default EffortList;
+
+const EffortListComponent = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 3rem;
+`;
+
+const CardListComponent = styled.div`
+  width: 1160px;
+  display: grid;
+  grid-template-rows: repeat(1, 1fr);
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 2rem;
+  margin-top: 2rem;
+`;
