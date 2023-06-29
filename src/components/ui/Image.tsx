421aa66ef7789522a9ba058ef324d4e87d5060a9
Soye Park <stardust6653@gmail.com>
2023-06-29 23:59:06 +0900

    feat : 재사용을 위한 Image 컴포넌트 분리

---
+++ src/components/ui/Image.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { styled } from "styled-components";
+
+type Props = {
+  src: string | undefined;
+  alt: string;
+  onClick?: (src: string) => void;
+};
+
+const Image = ({ src, alt, onClick }: Props) => {
+  return (
+    <ImageComponent
+      src={src}
+      alt={alt}
+      onClick={() => {
+        if (src) {
+          onClick?.(src);
+        }
+      }}
+    />
+  );
+};
+
+export default Image;
+
+const ImageComponent = styled.img`
+  margin-top: 2rem;
+  width: 100%;
+  border-bottom: 1px solid black;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+`;
