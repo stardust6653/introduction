01a2bdf6e2f47477ff9b3ed4562283c7405b6a12
Soye Park <stardust6653@gmail.com>
2023-06-29 23:57:45 +0900

    data : Effort Data 추가

---
+++ src/data/effortData.ts
@@ -0,0 +1,22 @@
+export type EffortProps = {
+  title: string;
+  description: string;
+};
+
+export const effortData: EffortProps[] = [
+  {
+    title: "스터디로 동기부여!",
+    description:
+      "모던JS튜토리얼, 코드스피츠 스터디를 통해 JS에 대한 기초와 심화를 학습했습니다. 또한 CS스터디와 알고리즘 스터디를 통해 CS지식을 쌓아올리고 있습니다!",
+  },
+  {
+    title: "기록이 성장의 원동력",
+    description:
+      '공부하거나 경험한 것을 “내가 이해하기 쉬운 문장"으로 바꾸어 기록하는 것은 개발자로서 든든한 자원이라고 생각해 꾸준하게 블로깅합니다.',
+  },
+  {
+    title: "공식문서가 정확하죠!",
+    description:
+      "최근에는 대부분의 라이브러리 혹은 프레임워크들에서 제공하는 문서가 혼자서 학습하기에 지장없을 만큼이나 자세하게 작성되어있습니다. 공식문서를 통해 학습하는 것을 습관화 합니다.",
+  },
+];
