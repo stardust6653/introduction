dc3e71133891bbd3cfd2690df85c180b047a69c4
Soye Park <stardust6653@gmail.com>
2023-06-29 23:59:39 +0900

    feat :  ProjectCard 미리 보기 기능 추가 및 스타일링 추가

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -1,68 +1,148 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { styled } from "styled-components";
 import { projectsData } from "data/projectsData";
+import Image from "./ui/Image";
 
-const ProjectCard = () => {
+type Props = {
+  cardIndex: number;
+};
+
+const ProjectCard = ({ cardIndex }: Props) => {
+  const card = projectsData[cardIndex];
+  const imageArr = card.screenshot;
+  const [currentImg, setCurrentImg] = useState(imageArr[0]);
+
+  useEffect(() => {
+    setCurrentImg(imageArr[0]);
+  }, [cardIndex]);
+
   return (
-    <div>
-      {projectsData.map((item) => {
-        return (
+    <ProjectCardComponent>
+      <NameComponent>
+        <CardTitle>{card.projectName}</CardTitle>
+        <LinkGroup>
+          {card.github && (
+            <Link href={card.github} target="_blank">
+              GITHUB
+            </Link>
+          )}
+          {card.notion && (
+            <Link href={card.notion} target="_blank">
+              NOTION
+            </Link>
+          )}
+          {card.deploy && (
+            <Link href={card.deploy} target="_blank">
+              DEPLOY
+            </Link>
+          )}
+        </LinkGroup>
+
+        {
           <>
-            <div>
-              <h2>{item.projectName}</h2>
-              <a href={item.github}>github</a>
-              <a href={item.notion}>notion</a>
-              <a href={item.deploy}>deploy</a>
-            </div>
-            <div>
-              <h3>프로젝트 소개</h3>
-              <p>{item.projectIntro}</p>
-            </div>
-            <div>
-              <h3>프로젝트 구현 기능</h3>
-              {item.myTask.map((item) => {
+            <ImageComponent>
+              <Image src={currentImg} alt={`${card.projectName} 스크린샷`} />
+            </ImageComponent>
+            <AnotherImageList>
+              {card.screenshot.map((item) => {
                 return (
-                  <>
-                    <h4>{item.task}</h4>
-                    <p>{item.description}</p>
-                  </>
+                  <Image
+                    src={item}
+                    alt={`${card.projectName} 스크린샷`}
+                    onClick={setCurrentImg}
+                  />
                 );
               })}
-            </div>
-            <div>
-              <h3>사용스택</h3>
-              {item.useStack.map((item) => {
-                return <p>{item}</p>;
-              })}
-            </div>
-            <div>
-              <h3>이 스택은 이런 이유로 사용했어요!</h3>
-              {item.whyUseStack?.map((item) => {
-                return (
-                  <>
-                    <h4>{item.stack}</h4>
-                    {item.reason.map((item) => (
-                      <p>{item}</p>
-                    ))}
-                  </>
-                );
-              })}
-            </div>
-            <div>
-              <h4>개발하면서 이런 고민들을 했어요</h4>
-              {item.posts?.map((item) => {
-                return (
-                  <p>
-                    <a href={item.link}>블로그 포스팅 : {item.title}</a>
-                  </p>
-                );
-              })}
-            </div>
+            </AnotherImageList>
           </>
-        );
-      })}
-    </div>
+        }
+      </NameComponent>
+
+      <IntroductionComponent>
+        <div>
+          <ListTitle>프로젝트 소개</ListTitle>
+          <Description>{card.projectIntro}</Description>
+        </div>
+
+        <div>
+          {card.useStack.map((item) => {
+            return <UseStack>#{item}</UseStack>;
+          })}
+        </div>
+      </IntroductionComponent>
+    </ProjectCardComponent>
   );
 };
 
 export default ProjectCard;
+
+const ProjectCardComponent = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 2rem;
+  width: 1160px;
+  border-bottom: 1px solid black;
+  border-radius: 10px;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 60px 80px;
+  background-color: #f8f8f8;
+`;
+
+const NameComponent = styled.div``;
+
+const CardTitle = styled.h2`
+  font-family: "Anybody", cursive;
+  font-size: 3rem;
+  margin-bottom: 1.3rem;
+`;
+
+const UseStack = styled.span`
+  display: inline-block;
+  margin: 0 1rem 1.5rem 0;
+  border: 1px solid black;
+  border-radius: 10px;
+  padding: 10px;
+  text-align: center;
+`;
+
+const LinkGroup = styled.div``;
+
+const Link = styled.a`
+  text-align: center;
+  font-size: 1.3rem;
+  margin: 0 30px 20px 0;
+  &:hover {
+    border-bottom: 2px solid black;
+    font-weight: 700;
+  }
+`;
+
+const ListTitle = styled.h3`
+  color: #172027;
+  font-size: 2rem;
+  margin-bottom: 1.5rem;
+`;
+
+const Description = styled.p`
+  line-height: 1.5rem;
+  margin-bottom: 1.5rem;
+  font-size: 1.2rem;
+`;
+
+const ImageComponent = styled.div`
+  width: 100%;
+  position: relative;
+`;
+
+const AnotherImageList = styled.div`
+  display: grid;
+  grid-template-rows: repeat(1, 1fr);
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  margin-bottom: 2rem;
+`;
+
+const IntroductionComponent = styled.div`
+  width: 100%;
+`;
